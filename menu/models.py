300611f162409d19a1882cf172df64a1aec257fc
Łukasz Piotrkowski <lukasz.piotrkowski@profil-software.com>
2018-11-23 18:11:57 +0100

    Add fixtures, change default verbose names for django admin

--- menu/models.py
+++ menu/models.py
@@ -6,29 +6,35 @@ from helpers import get_image_path
 class BaseModel(models.Model):
     creation_date = models.DateTimeField(auto_now_add=True)
     modification_date = models.DateTimeField(auto_now=True)
-    description = models.CharField(max_length=500)
+    description = models.CharField('Opis', max_length=500)
 
     class Meta:
         abstract = True
 
 
 class Menu(BaseModel):
-    name = models.CharField(max_length=200, unique=True)
-    dishes = models.ManyToManyField('menu.Dish')
+    name = models.CharField('Nazwa', max_length=200, unique=True)
+    dishes = models.ManyToManyField('menu.Dish', verbose_name='Potrawy')
 
     class Meta:
+        verbose_name_plural = 'Karty menu'
         ordering = ('id',)
 
-    def __str__(self):
-        return f'{self.name}'
+
+def __str__(self):
+    return f'{self.name}'
 
 
 class Dish(BaseModel):
-    name = models.CharField(max_length=200, unique=True)
-    preparation_time = models.IntegerField()
-    is_vegetarian = models.BooleanField(default=False)
-    price = models.DecimalField(decimal_places=2, max_digits=5)
-    picture = models.ImageField(upload_to=get_image_path, null=True, blank=True, )
+    name = models.CharField('Nazwa', max_length=200, unique=True)
+    preparation_time = models.IntegerField('Czas przygotowania')
+    is_vegetarian = models.BooleanField('Potrawa wegetariańska?', default=False)
+    price = models.DecimalField('Cena', decimal_places=2, max_digits=5)
+    picture = models.ImageField('Obrazek', upload_to=get_image_path, null=True, blank=True, )
+
+    class Meta:
+        verbose_name = 'Potrawa'
+        verbose_name_plural = 'Potrawy'
 
     def __str__(self):
         return f'{self.name}, {self.price}'
